a0e91f007b5b7fbb12824258858c6a50715c34b8
gungwah <gungwah.amitaba@gmail.com>
2024-11-02 22:20:16 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import langchain as langchain
 from langchain_community.llms import OpenAI 
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableBranch
 from langchain.prompts import PromptTemplate
 import os
+
+langchain.debug = True
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
 
@@ -20,19 +23,20 @@ cause_template = """If the experience is negative, determine the cause:
 Only respond with "airline fault" or "external factors".
 """
 
-sentiment_chain = PromptTemplate.from_template(sentiment_template) | llm 
-cause_chain = PromptTemplate.from_template(cause_template) | llm 
+sentiment_chain = PromptTemplate.from_template(sentiment_template) | llm | StrOutputParser()
+cause_chain = PromptTemplate.from_template(cause_template) | llm | StrOutputParser()
 
 
-positive_response = PromptTemplate.from_template("Thank you for choosing our airline! We're glad you had a good experience.") | llm
-airline_fault_response = PromptTemplate.from_template("We apologize for the inconvenience. Our customer service team will contact you soon to resolve the issue.") | llm
-external_factors_response = PromptTemplate.from_template("We’re sorry for your experience. Unfortunately, the airline is not liable for events outside of our control.") | llm
+positive_response = PromptTemplate.from_template("thank them for their feedback and for choosing to fly with the airline.") | llm | StrOutputParser()
+airline_fault_response = PromptTemplate.from_template("display a message offering sympathies and inform the user that customer service will contact them soon to resolve the issue or provide compensation.") | llm | StrOutputParser()
+external_factors_response = PromptTemplate.from_template("display a message implying sympathy but explain that the airline is not liable in such situations.") | llm | StrOutputParser()
 
 feedback_branch = RunnableBranch(
     (lambda x: "positive" in x["sentiment"].lower(), positive_response),
     (lambda x: "negative" in x["sentiment"].lower() and "airline fault" in x["cause"].lower(), airline_fault_response),
     external_factors_response,
 )
+
 
 full_chain = {"sentiment": sentiment_chain, "cause": cause_chain, "feedback": lambda x: x["feedback"]} | feedback_branch
 
